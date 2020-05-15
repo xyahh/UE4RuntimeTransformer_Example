f5aa985c476f7cc8f8fb84d174659a4caca29505
Juan <juan@xyah.games>
2020-05-16 03:01:16 +0900

    Removed Plugins folder. Download the correct version of the plugin in the Runtime Transformer Repository

--- Plugins/RuntimeTransformer/Source/RuntimeTransformer/Private/FocusableObject.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright 2020 Juan Marcelo Portillo. All Rights Reserved.
-
-
-#include "FocusableObject.h"
-
-// Add default functionality here for any IFocusableObject functions that are not pure virtual.
